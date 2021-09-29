9cdd34ffb5ff5aa75f66efd53eba8fabd011f51f
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2021-09-29 22:02:24 +0200

    Add padding

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,18 +6,20 @@ export default function Index() {
     <Layout>
       <div className="my-4">
         <h2 class="text-center">Who are we?</h2>
-        <h3><a href="https://github.com/SapphireDisD">Sapphire</a></h3>
-        <p>Ranker's author.</p>
-        <h3><a href="https://github.com/dongle-the-gadget">Dongle</a></h3>
-        <p>Ranker's main dev.</p>
-        <h3><a href="https://github.com/Ahmed605">Ahemd Walid</a></h3>
-        <p>Ranker's main rank card main developer.</p>
-        <h3><a href="https://github.com/zeealeid">Zeealeid</a></h3>
-        <p>Ranker's main rank card main designer.</p>
-        <h3><a href="https://github.com/KojiOdyssey">Koji</a>, <a href="https://github.com/dAKirby309">Kirby</a>, <a href="https://github.com/itsWindows11">SimpleBear</a></h3>
-        <p>Ranker's main rank card design collaborators.</p>
-        <h3><a href="https://discord.com/users/188482204601548800">Fleuron</a></h3>
-        <p>Ranker's second rank card designer.</p>
+        <div className="padding-container">
+          <h3><a href="https://github.com/SapphireDisD">Sapphire</a></h3>
+          <p>Ranker's author.</p>
+          <h3><a href="https://github.com/dongle-the-gadget">Dongle</a></h3>
+          <p>Ranker's main dev.</p>
+          <h3><a href="https://github.com/Ahmed605">Ahemd Walid</a></h3>
+          <p>Ranker's main rank card main developer.</p>
+          <h3><a href="https://github.com/zeealeid">Zeealeid</a></h3>
+          <p>Ranker's main rank card main designer.</p>
+          <h3><a href="https://github.com/KojiOdyssey">Koji</a>, <a href="https://github.com/dAKirby309">Kirby</a>, <a href="https://github.com/itsWindows11">SimpleBear</a></h3>
+          <p>Ranker's main rank card design collaborators.</p>
+          <h3><a href="https://discord.com/users/188482204601548800">Fleuron</a></h3>
+          <p>Ranker's second rank card designer.</p>
+        </div>
       </div>
     </Layout>
   )

3c0ac15ef3adcae7af226bccf6f301b709369efa
SapphireDisD <villagerlover52@hotmail.com>
2021-10-03 10:39:04 -0300

    Added initial leaderboard (just misses rank card customization)

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import SEO from './SEO'
 import Header from "./header"
 
-const Layout = ({ children, customSEO }) => {
+const Layout = ({ children}) => {
     const data = useStaticQuery(graphql`
      query SiteTitleQuery {
        site {
@@ -22,7 +21,6 @@ const Layout = ({ children, customSEO }) => {
 
     return (
         <>
-        {!customSEO && <SEO />}
             <Header menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
             <div className="content">{children}</div>
         </>

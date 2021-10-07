0ecc7826a5a88c51812576a3385f98e8b588c372
SapphireDisD <villagerlover52@hotmail.com>
2021-10-07 09:11:08 -0300

    Removed add to Discord

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button'
+import { DefaultButton } from '@fluentui/react/lib/Button'
 import { navigate } from "@reach/router"
 import '../styles/CustomStyles.css'
 import Seo from '../components/seo'
@@ -12,7 +12,6 @@ export default function Index() {
       <div className="text-center my-4 padding-container">
         <h2>Rank your Discord server members with Ranker!</h2>
         <p>Ranker is a ranking Discord bot, it offers a list of members, ordered by XP called leaderboard, it also offers a command to show the rank of a specific user and a system to add roles.</p>
-        <PrimaryButton text="Add to Discord" onClick={() => navigate('https://discord.com/api/oauth2/authorize?client_id=889281672988749855&permissions=378225675264&scope=bot%20applications.commands')} className="me-1" />
         <DefaultButton text="GitHub" onClick={() => navigate('https://github.com/Ranker-Team/Ranker')} />
       </div>
     </Layout>
